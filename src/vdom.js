2d6b7b145f266ba4ac300cd49a638e4b3d815f3b
*nikki <64@live.jp>
2019-02-22 23:55:34 +0900

    wip: stable

--- src/vdom.js
+++ src/vdom.js
@@ -10,12 +10,13 @@ const state = {
 };
 
 const action = {
-  onkeydown: ({keyCode, target})=> (state)=> {
+  oninput: ({target})=> ()=> ({input: target.value}),
+  onkeydown: ({keyCode})=> (state)=> {
     const enterKeyCode = 13;
     const upKeyCode = 38;
     const downKeyCode = 40;
     switch (keyCode) {
-      case enterKeyCode: return target.value !== "" ? action.eval({...state, input: target.value}) : {};
+      case enterKeyCode: return action.eval(state);
       case upKeyCode: return action.prev(state);
       case downKeyCode: return action.next(state);
     }
@@ -24,27 +25,28 @@ const action = {
   onupbuttonpress: ()=> (state)=> action.prev(state),
   onenterbuttonpress: ()=> (state)=> action.eval(state),
 
-  prev: (state)=> {
-    let previndex = Math.min(state.inputHistory.length-1, state.historyIndex+1);
+  prev: ({historyIndex, inputHistory})=> {
+    let prevIndex = Math.min(historyIndex + 1, inputHistory.length - 1);
     return {
-      historyIndex: previndex,
-      input: state.inputHistory[previndex],
+      historyIndex: prevIndex,
+      input: inputHistory[prevIndex],
     }
   },
 
-  next: (state)=> {
-    let nextIndex = Math.max(0, state.historyIndex-1);
+  next: ({historyIndex, inputHistory})=> {
+    let nextIndex = Math.max(0, historyIndex - 1);
     return {
       historyIndex: nextIndex,
-      input: state.inputHistory[nextIndex],
+      input: inputHistory[nextIndex],
     }
   },
 
-  eval: (state)=> {
+  eval: ({input, inputHistory, maxInputHistory, logHistory, maxLogHistory})=> {
+    if (input === "") return;
     return {
       input: "",
-      inputHistory: [state.input, ...state.inputHistory],
-      logHistory: [...state.logHistory, "eval(" + state.input + ")"],
+      inputHistory: [input, ...inputHistory].slice(0, maxInputHistory),
+      logHistory: [...logHistory, "eval(" + input + ")"].slice(Math.max(0, logHistory.length+1 - maxLogHistory)),
       historyIndex: -1,
     }
   },
@@ -52,26 +54,26 @@ const action = {
 
 let stringify = (object)=> {
   switch (object) {
-    case undefined:
-    object = "undefined";
-    break;
-
-    case null:
-    object = "null";
-    break;
+    case undefined: return "undefined";
+    case null: return "null";
+    default: return object;
   }
-
-  return object;
 }
 
-const view = (state, action)=>
+const view = ({input, logHistory}, {oninput, onkeydown, onupbuttonpress, onenterbuttonpress})=>
   h("div", {}, [
-    h("textarea", {class: "logbox", readonly: true}, state.logHistory.map((item)=> stringify(item)).join("\n")),
+    h("textarea", {class: "logbox", readonly: true}, logHistory.map((log)=> stringify(log)).join("\n")),
     h("label", {},
-      h("input", {class: "input", type: "text", value: state.input, onkeydown: (event)=> action.onkeydown(event)}),
+      h("input", {
+        class: "input",
+        type: "text",
+        value: input,
+        oninput: (event)=> oninput(event),
+        onkeydown: (event)=> onkeydown(event),
+      }),
     ),
-    h("button", {class: "up", onclick: ()=> action.onupbuttonpress()}, "↑"),
-    h("button", {class: "enter", onclick: ()=> action.onenterbuttonpress()}, ">>"),
+    h("button", {class: "up", onclick: ()=> onupbuttonpress()}, "↑"),
+    h("button", {class: "enter", onclick: ()=> onenterbuttonpress()}, ">>"),
   ])
 
 export default (node)=> app(state, action, view, node);
